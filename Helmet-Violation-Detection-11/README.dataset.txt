ae118e6c2bac5577aeb5b399a0094ffa2fd93914
Aabidha <aamarijames6@gmail.com>
2026-06-23 23:42:14 +0530

    Final hackathon submission

---
+++ Helmet-Violation-Detection-11/README.dataset.txt
@@ -0,0 +1,6 @@
+# Helmet Violation Detection > 2025-06-30 12:24am
+https://universe.roboflow.com/radya-ai/helmet-violation-detection-zgzh1
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+
+    allowedHosts: [
+      "renderer-sprig-deputy.ngrok-free.dev"
+    ],
+
     port: 5173,
+
     proxy: {
       "/api": {
         target: "http://localhost:8000",
